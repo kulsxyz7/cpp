eb0c65a4b2ea465e5e01af40e5a30760d4e38ff0
kulsxyz7 <kulsxyz7@gmail.com>
2019-11-17 22:34:26 +0700

    menambahkan file 3x5.cpp

--- 3x5.cpp
+++ 3x5.cpp
@@ -7,48 +7,39 @@ int main()
 {
     int a = 3;
     int b = 5;
-    int x, z, matrik[a][b], matriks[a][b];
-
-    cout << "\tMatriks 1 " << endl;
-    for (x = 0; x < a; x++)
-    {
-        for (z = 0; z < b; z++)
-        {
-            cout < " Input Matriks 1 (" << x << "," << z << ") = ";
-            cin >> matrik[x][z];
+    int x,z,matrik[a][b],matriks[a][b];
+    
+    cout<<"\tMatriks 1 "<<endl;
+    for ( x=0;x<a;x++ ) {
+        for ( z=0;z<b;z++ ) {
+            cout<<" Input Matriks 1 ("<<x<<","<<z<<") = ";
+            cin>>matrik[x][z];
         }
     }
 
-    cout << endl;
-    cout << "\tMatriks Pertama = " << endl;
-    for (x = 0; x < a; x++)
-    {
-        for (z = 0; z < b; z++)
-        {
-            cout << matrik[x][z];
-            cout << " ";
+    cout<<endl;
+    cout<<"\tMatriks Pertama = "<<endl;
+    for ( x=0;x<a;x++ ) {
+        for (  z=0;z<b;z++ ) {
+            cout<<matrik[x][z];
+            cout<<" ";
         }
-        cout << "\n";
+        cout<<"\n";
     }
 
-    for (x = 0; x < a; x++)
-    {
-        for (z = 0; z < b; z++)
-        {
-            matriks[z][x] = matrik[x][z];
+    for ( x=0;x<a;x++ ) {
+        for ( z=0;z<b;z++ ) {
+            matriks [z][x]=matrik[x][z];
         }
     }
 
-    cout << endl
-         << "Hasil Transpose Matriks = " << endl;
-    for (z = 0; z < b; z++)
-    {
-        for (x = 0; x < a; x++)
-        {
-            cout << matriks[z][x];
-            cout << " ";
+    cout<<endl<<"Hasil Transpose Matriks = "<<endl;
+    for ( z=0;z<b;z++ ) {
+        for ( x=0;x<a;x++ ) {
+            cout<<matriks[z][x];
+            cout<<" ";
         }
-        cout << "\n";
+        cout<<"\n";
     }
 
     return 0;
