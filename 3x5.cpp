4c2ae7d360cd12ca8bb1dd4944cdf281a1ea8b10
kulsxyz7 <kulsxyz7@gmail.com>
2019-11-17 22:22:23 +0700

    mengisi file 3x5.cpp

--- 3x5.cpp
+++ 3x5.cpp
@@ -5,4 +5,51 @@ using namespace std;
 
 int main()
 {
+    int a = 3;
+    int b = 5;
+    int x, z, matrik[a][b], matriks[a][b];
+
+    cout << "\tMatriks 1 " << endl;
+    for (x = 0; x < a; x++)
+    {
+        for (z = 0; z < b; z++)
+        {
+            cout < " Input Matriks 1 (" << x << "," << z << ") = ";
+            cin >> matrik[x][z];
+        }
+    }
+
+    cout << endl;
+    cout << "\tMatriks Pertama = " << endl;
+    for (x = 0; x < a; x++)
+    {
+        for (z = 0; z < b; z++)
+        {
+            cout << matrik[x][z];
+            cout << " ";
+        }
+        cout << "\n";
+    }
+
+    for (x = 0; x < a; x++)
+    {
+        for (z = 0; z < b; z++)
+        {
+            matriks[z][x] = matrik[x][z];
+        }
+    }
+
+    cout << endl
+         << "Hasil Transpose Matriks = " << endl;
+    for (z = 0; z < b; z++)
+    {
+        for (x = 0; x < a; x++)
+        {
+            cout << matriks[z][x];
+            cout << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
 }
