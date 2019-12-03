f1626776b7da8349271495c4005ce5a7019a8335
kulsxyz7 <kulsxyz7@gmail.com>
2019-12-03 07:34:55 +0700

    menambahkan file insertion-sort.cpp

---
+++ insertion-sort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+using namespace std;
+
+void insertionSort(int A[], int n)
+{
+
+    for (int i = 0; i < n - 1)
+}
+
+int main()
+{
+
+    int A[] = {23, 1, 5, 43, 8, 12};
+
+    cout << "Data Sebelum di Bubble Sort = " << endl;
+    for (int i = 0; i < 6; i++)
+    {
+        cout << A[i] << "  ";
+    }
+    cout << endl
+         << endl;
+}
