f6e26ee2de4abb0b30dad495fce1abc7fe1602c8
kulsxyz7 <kulsxyz7@gmail.com>
2019-12-03 21:24:47 +0700

    mengubah isi file insertion-sort.cpp

--- insertion-sort.cpp
+++ insertion-sort.cpp
@@ -4,22 +4,41 @@
 #include <conio.h>
 using namespace std;
 
+// Function untuk sorting Array
 void insertionSort(int A[], int n)
 {
+    int hole,i,j;
+    for ( i = 0; i < n - 1; i++ ) {
+        hole = A[i];
+        j=i-1;
+        // memindahkan element dari arr[0...i-1]
+        // yang lebih besar dari int hole, dari posisi didepan nya
+        // ke posisi sebelumnya
 
-    for (int i = 0; i < n - 1)
+        while ( j>0 && A[j] > hole) {
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = hole;
+    }
+
 }
 
 int main()
 {
 
-    int A[] = {23, 1, 5, 43, 8, 12};
+    int A[] = {23, 1, 5, 43, 8, 7};
 
-    cout << "Data Sebelum di Bubble Sort = " << endl;
+    cout << "Data Sebelum di Sort = " << endl;
     for (int i = 0; i < 6; i++)
     {
         cout << A[i] << "  ";
     }
     cout << endl
          << endl;
+    insertionSort(A,6);
+    cout<<"Data Sesudah di Sorting = "<<endl;
+    for ( int i = 0; i < 6; i++ ) {
+        cout<< A[i]<<" ";
+    }
 }
